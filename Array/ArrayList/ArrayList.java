26508ef06f2c192da744a2d92a9b6eeaed3af2d4
Ranajit_Khandual <ranajitkhandual@gmail.com>
2022-01-15 20:29:27 +0530

    Create ArrayList.java

---
+++ Array/ArrayList/ArrayList.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+ 
+public class ArrayList{
+   public static void main(String args[]) {
+    ArrayList<Integer> list = new ArrayList<>();
+    //add elements
+    list.add(0);  //index[0]
+    list.add(2);  //index[1]
+    list.add(3);  //index[2]
+    System.out.println(list);
+
+    //geting elements from specific index
+    int element = list.get(2);
+    System.out.println(element);
+
+    //add elements in between two index
+    list.add(1, 1);  //list.add(index[number], element)
+    System.out.println(list);
+
+    //set elements in array list
+
+    list.set(0, 5);
+    System.out.println(list);
+
+    //delete elements in array list
+    list.remove(3);
+    System.out.println(list);
+
+    //size of elements in array
+    int size = list.size();
+    System.out.println(size);
+
+    //loops
+    for(int i =0; i<list.size(); i++){
+       System.out.println(list.get(i));
+    }
+    //sorting
+    Collections.sort(list);
+    System.out.println(list);
+   }
+}
