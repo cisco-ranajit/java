07537aa5208e5134756e22a009d904a19a662364
Ranajit_Khandual <ranajitkhandual@gmail.com>
2022-01-17 12:21:43 +0530

    Create Parent.java
    Inheritance in Java is a mechanism in which one object acquires 
    all the properties and behaviors of a parent object. 
    It is an important part of OOPs (Object Oriented programming system).

---
+++ Inheritance/Parent.java
@@ -0,0 +1,17 @@
+//use of inheritance is java ( advantage - code reuseable )
+class Parent{
+    void show(){
+        System.out.println("Base class Method");
+    }
+}class Child extends Parent{
+    void display(){
+        System.out.println("Derived class Method");
+    }
+}
+class DemoInheritance{
+    public static void main(String[] args) {
+        Child ob = new Child();
+        ob.display();
+        ob.show();
+    }
+}
