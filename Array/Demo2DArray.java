30ea1cd130ea006b1df80051a670c9090552a151
Ranajit_Khandual <ranajitkhandual@gmail.com>
2022-01-25 15:13:09 +0530

    Update Demo2DArray.java

--- Array/Demo2DArray.java
+++ Array/Demo2DArray.java
@@ -116,10 +116,98 @@ public class Demo2DArray {
 
     }
 
+    void Addition() {
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the base of squared matrices");
+        n = input.nextInt();
+        int[][] a = new int[n][n];
+        int[][] b = new int[n][n];
+        int[][] c = new int[n][n];
+        System.out.println("Enter the elements of 1st martix row wise \n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = input.nextInt();
+            }
+        }
+        System.out.println("Enter the elements of 2nd martix row wise \n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                b[i][j] = input.nextInt();
+            }
+        }
+        System.out.println("Multiplying the matrices...");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }
+        input.close();
+    }
+
+    void Substraction() {
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the base of squared matrices");
+        n = input.nextInt();
+        int[][] a = new int[n][n];
+        int[][] b = new int[n][n];
+        int[][] c = new int[n][n];
+        System.out.println("Enter the elements of 1st martix row wise \n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = input.nextInt();
+            }
+        }
+        System.out.println("Enter the elements of 2nd martix row wise \n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                b[i][j] = input.nextInt();
+            }
+        }
+        System.out.println("Substracting the matrices...");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                c[i][j] = a[i][j] - b[i][j];
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }
+        input.close();
+    }
+
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Demo2DArray ob;
         ob = new Demo2DArray();
-        ob.multiplicationOfArray();
+        System.out.println("1. Declaration of 2-D Array");
+        System.out.println("2. Multiplicaion of 2-D Array (Matrix");
+        System.out.println("3. Additon of 2-D Array (Matrix)");
+        System.out.println("4. Substraction of 2-D Array (Matrix)");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("choose between 1 to 3");
+        int number = input.nextInt();
+        switch (number) {
+            case 1:
+                ob.DeclarationOf2DArray();
+                break;
+            case 2:
+                ob.multiplicationOfArray();
+                break;
+            case 3:
+                ob.Addition();
+                break;
+            case 4:
+                ob.Substraction();
+                break;
+            default:
+                System.out.println("you entered invalid number, please enter number 1 t0 3");
+                break;
+        }
+
     }
 
 }
