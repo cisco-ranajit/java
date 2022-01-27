0093cf35dcb9a773eec6f7b97fa5eb2b624b69f8
Ranajit_Khandual <ranajitkhandual@gmail.com>
2022-01-27 21:43:57 +0530

    Add files via upload

--- Array/ArrayOperation.java
+++ Array/ArrayOperation.java
@@ -1,24 +1,67 @@
-import java.util.Random;
-import java.util.Scanner;
-import java.util.Random;
-public class ArrayOperation {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int size = input.nextInt();
-        int age[] = new int[size];
-
-
-        for(int i = 0; i < age.length; i++){
-            age[i] = input.nextInt();
-        }
-        for(int i = 0; i < size; i++){
-            System.out.println(age[i]);
-        }
-        //get the first and last element of an array
-        System.out.println("first element of array "+age[0]);
-        System.out.println("last element of array "+age[age.length -1]);
-        //get a random value from an array
-        int anyValue = age[new Random(). nextInt(size)];
-        System.out.println(anyValue);
-    }
+import java.util.*;
+class ArrayOperation{
+	Scanner input = new Scanner(System.in);
+	
+	void InsertArray(){
+		System.out.println("enter size of array");
+		int size = input.nextInt();
+	    int arr[] = new int[size + 1];
+		System.out.println("enter elements of array");
+		for( int i = 0; i < size; i++){
+			arr[i] = input.nextInt();
+		}
+		System.out.println("the elements of array");
+		for(int i = 0; i < size; i++){
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		System.out.println("enter the new element");
+		int new_element = input.nextInt();
+		System.out.println("enter the position of array where you want to insert");
+		int position = input.nextInt();
+		for(int i = size; i >= position; i--){
+			arr[i] = arr[i - 1 ];
+		} arr [position - 1] = new_element;
+			for( int i = 0;i <= size; i++){
+				System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+	void DeleteElement(){
+		System.out.println("size of array");
+		
+		int size = input.nextInt();
+		int arr[] = new int[size];
+		
+		System.out.println("enter elements of array");
+		for( int i = 0; i < size; i++){
+			arr[i] = input.nextInt();
+		}
+		
+		System.out.println("the elements of array");
+		for(int i = 0; i < size; i++){
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		
+		System.out.println("enter the element which you want to delete");
+		int delete = input.nextInt();
+		for( int i = 0; i < size - 1; i++){
+			if(arr[i] == delete){
+				for(int j = i; j < size - 1; j++){
+					arr[j] = arr[j + 1];
+				}
+			}
+		}
+		System.out.println("deleted element"+delete);
+		for(int i = 0 ; i < size-1; i++){
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+	public static void main(String args[]){
+		ArrayOperation ob = new ArrayOperation();
+		ob.InsertArray();
+		ob.DeleteElement();
+	}
 }
